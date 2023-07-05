c36ec9190af5c3eb377c9dc4b1b7d6a65d9e736f
okkDevelop <127747248+okkDevelop@users.noreply.github.com>
2023-07-06 00:13:28 +0800

    Update AudioManager.cs

--- Monster Festival Collection/AudioManager.cs
+++ Monster Festival Collection/AudioManager.cs
@@ -14,6 +14,7 @@ public class AudioManager : MonoBehaviour
 			s.source = gameObject.AddComponent<AudioSource>();
 			s.source.clip = s.clip;
 			s.source.loop = s.loop;
+   			s.source.volume = s.volume;
 		}
 				
     }
